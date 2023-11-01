b23ab8dd0b18f3b322e5c7d8fa00b529bd79fe95
Magdalena <Amagdalena.C@gmail.com>
2023-10-31 19:18:32 -0500

    CountryApp done

---
+++ src/app/countries/services/countries.service.ts
@@ -0,0 +1,76 @@
+
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable, catchError, map, of } from 'rxjs';
+import { Country } from '../interfaces/country';
+
+@Injectable({providedIn: 'root'})
+export class CountriesService {
+
+  private apiUrl: string = 'https://restcountries.com/v3.1'
+
+
+  constructor(private http: HttpClient) { }
+
+  searchCountryByAlphaCode( code: string ): Observable<Country | null> {
+
+    const url = `${ this.apiUrl }/alpha/${ code }`;
+
+    return this.http.get<Country[]>( url )
+      .pipe(
+        map( countries => countries.length > 0 ? countries[0]: null ),
+        catchError( error => {
+          console.log( error );
+
+          return of(null)
+        })
+      );
+  }
+
+
+  searchCapital( term: string ): Observable<Country[]> {
+
+    // return this.http.get<Country[]>(`${ this.apiUrl }/capital/${ term }`);
+
+    const url = `${ this.apiUrl }/capital/${ term }`;
+
+    return this.http.get<Country[]>( url )
+      .pipe(
+        /* tap( countries => console.log( 'Tap1', countries )),
+        map( countries => [] ),
+        tap( countries => console.log( 'Tap2', countries )), */
+
+        catchError( error => {
+          console.log( error );
+
+          return of([])
+        })
+      );
+  }
+
+  searchCountry( term: string ): Observable<Country[]> {
+    const url = `${ this.apiUrl }/name/${ term }`;
+
+    return this.http.get<Country[]>( url )
+      .pipe(
+        catchError( error => {
+          console.log( error );
+
+          return of([])
+        })
+      );
+  }
+
+  searchRegion( term: string ): Observable<Country[]> {
+    const url = `${ this.apiUrl }/region/${ term }`;
+
+    return this.http.get<Country[]>( url )
+      .pipe(
+        catchError( error => {
+          console.log( error );
+
+          return of([])
+        })
+      );
+  }
+}
